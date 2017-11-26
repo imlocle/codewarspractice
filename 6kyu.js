057b62d822c27ba8b0bf7c446b78046006bbd720
Loc Le <loc.tan.le@gmail.com>
2017-11-26 12:40:15 -0800

    organizing by kyu

--- 6kyu.js
+++ 6kyu.js
@@ -1,32 +1,3 @@
-// https://www.codewars.com/kata/56541980fa08ab47a0000040/train/javascript
-// 7 kyu
-// In a factory a printer prints labels for boxes.
-// For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
-// The colors used by the printer are recorded in a control string. 
-// For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
-// Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm.
-// You have to write a function printer_error which given a string will output the error rate of the printer as a string
-// representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
-// The string has a length greater or equal to one and contains only letters from a to z.
-function printerError(string){
-    var countError = 0;
-    // checking length and characters
-    if (string.length < 1 || string.match(/[\d+\W+]/i)){
-        return "Invalid";
-    }
-
-    else {
-        for (var i = 0; i < string.length; i++){
-            if (string[i].match(/[n-z]/i)){
-                countError++;
-            }
-        }
-    }
-    return countError + "/" + string.length;
-}
-
-printerError("aabbbeeee")
-
 // https://www.codewars.com/kata/speech-to-text-string-manipulation/train/javascript
 // 6 kyu
 // Siri needs you to code some new features: addition, subtraction and a weather checker. There's no need to implement the speech recognition, that's taken care of...
@@ -67,5 +38,6 @@ function message(msg){
         }
     }
 }
-    
-message("What is the weather at 6:00pm?")
+message("Add 5 to 20");    
+message("Subtract 13 from 439");
+message("What is the weather at 6:00pm?");
