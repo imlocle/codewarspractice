c08b0c7c9041a68f8d29ad732a77dc2b17bd2801
Loc Le <loc.tan.le@gmail.com>
2017-12-03 09:50:42 -0800

    duplicate encoder 6kyu

--- 6kyu.js
+++ 6kyu.js
@@ -1,3 +1,46 @@
+// https://www.codewars.com/kata/duplicate-encoder/train/javascript
+// 6 kyu
+// The goal of this exercise is to convert a string to a new string where each character in the new string is '(' if that character appears only once in the original string,
+// or ')' if that character appears more than once in the original string.
+// Ignore capitalization when determining if a character is a duplicate.
+
+// Examples:
+// "din" => "((("
+// "recede" => "()()()"
+// "Success" => ")())())"
+// "(( @" => "))(("
+
+// Notes:
+// There is a flaw in the JS version, that may occur in the random tests. Do not hesitate to do several attempts before modifying your code if you fail there. 
+// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is actually the expected result, not the input! (these languages are locked so that's not possible to correct it).
+
+function duplicateEncode(word){
+    var encodedString = "";
+    word = word.toLowerCase();
+    var dict = [];
+    for (var i = 0; i < word.length; i++){
+        dict.push({key:word[i], value:0});
+        for (var j = 0; j < word.length; j++){
+            if (word[i] == word[j]){
+                dict[i].value++;
+            }
+                     
+        }
+    }
+    for (var k = 0; k < dict.length; k++){
+       if (dict[k].value > 1){
+           encodedString += ")";
+       }
+       else {
+           encodedString += "(";
+       }
+    }
+    return encodedString;
+}
+
+duplicateEncode("(( @")
+
+
 // https://www.codewars.com/kata/speech-to-text-string-manipulation/train/javascript
 // 6 kyu
 // Siri needs you to code some new features: addition, subtraction and a weather checker. There's no need to implement the speech recognition, that's taken care of...
